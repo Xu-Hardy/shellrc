9b930e9355538878891a41153f5bbc6642a9eaaa
Xu <deep.work.max@gmail.com>
2024-02-08 08:47:09 +0800

    add nodejs alias

--- start.sh
+++ start.sh
@@ -1,4 +1,16 @@
 
-cp .src ~
-echo "source ~/.src" >> ~/.zshrc
+# set -x
+#!/bin/bash
+cp .shrc ~
+# 定义shrc文件的路径
+SHRC_PATH="~/.shrc"
+
+# 检查~/.zshrc中是否已经包含了SHRC_PATH的引用
+if grep -qF "source $SHRC_PATH" ~/.zshrc; then
+    echo "shrc已经被添加到~/.zshrc中了。"·
+else
+    # 如果没有找到，就添加一行引用到~/.zshrc的末尾
+    echo "source $SHRC_PATH" >> ~/.zshrc
+    echo "shrc已成功添加到~/.zshrc中。"
+fi
 source ~/.zshrc
